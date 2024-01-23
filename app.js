af9fc59c228f12dbe7adc564b92b04d5f1817a49
sinchana c poojary <sinchanac91@gmail.com>
2024-01-23 09:43:30 +0530

    🔧 chore(app.js): reformat code to improve readability and consistency ✨ feat(app.js): add root route to check if server is alive 🔒 chore(app.js): update mongoose connection options to improve compatibility and remove unnecessary whitespace

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const userRoute=require("./routes/user");
-const bodyParser=require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const bodyParser = require("body-parser");
 
-const app=express();
+const app = express();
 
 
 
-mongoose.connect("mongodb://localhost:27017/social_media",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-   
-    
+mongoose.connect("mongodb://localhost:27017/social_media", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-mongoose.connection.on("connected",()=>{
+mongoose.connection.on("connected", () => {
     console.log("connected to mongo yeahh");
 });
 
-mongoose.connection.on("error",(err)=>{
-    console.log("error",err);
+mongoose.connection.on("error", (err) => {
+    console.log("error", err);
 });
 
 app.use(bodyParser.json());
 
-app.use('/user',userRoute);  //user is the path
-const PORT=process.env.PORT||3000;
+app.get('/', (req, res) => {
+    res.send({
+        message: "Server is Alive"
+    });
+});
 
-app.listen(PORT,()=>{
-    console.log("server is running on",PORT);
+app.use('/user', userRoute);  //user is the path
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("server is running on", PORT);
+    console.log("http://localhost:" + PORT);
 });
